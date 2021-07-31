fe570af0b4aaa656cbc9a88235edd91a71935f6a
JimAnast <33915035+JimAnast@users.noreply.github.com>
2021-07-31 12:53:22 +0300

    Update word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -1,13 +1,13 @@
 import collections
 import copy
 
-class WordLadderSolve:           #class to generate the word ladder and solve the problem
+class WordLadderSolve:             #class to generate the word ladder and solve the problem
     def __init__(self, start):
-        self.ladder = [[start]]  #the word from which we are going to start the wordLadder
+        self.ladder = [[start]]    #the word from which we are going to start the wordLadder
 
-    def AddWord(self, word):     #We use this to append the possible adjacent words
+    def AddWord(self, word):       #We use this to append the possible adjacent words
         for item in self.ladder:
-            item.append(word)    #append the word to the item of the class
+            item.append(word)      #append the word to the item of the class
 
     def LastWord(self):            #We use this because we need to secure that
         return self.ladder[0][-1]  #the last word is the same is all lists in self.ladder
@@ -44,8 +44,8 @@ def BFS(start, end, dictionary):  #The arguments are the starting word, the dest
     explored = {start}
     # That queue contains items that are lists of the WordLaddersSolve class
     queue = collections.deque([WordLadderSolve(start)])
-    path = None  # this will contain the path of WordLadder
-    while queue:  # while the queue is not empty
+    path = None                            # this will contain the path of WordLadder
+    while queue:                           # while the queue is not empty
         #We are going to return all of the shortest paths. Therefore we ned to repeat
         #the same process for every level (number of queue items)
         num_level = len(queue)
@@ -53,23 +53,23 @@ def BFS(start, end, dictionary):  #The arguments are the starting word, the dest
         for i in range(num_level):  # go through all in this range.
             queue_element = queue.popleft()
             for node in PossibleAdjacents(queue_element.LastWord(), wordList):
-                if node in explored:  # if it has been explored, continue with the next
+                if node in explored:                        # if it has been explored, continue with the next
                     continue
-                new_ladder = copy.deepcopy(queue_element) #copy the element to the new_ladder
-                new_ladder.AddWord(node)                  #add it to the new_ladder
+                new_ladder = copy.deepcopy(queue_element)   #copy the element to the new_ladder
+                new_ladder.AddWord(node)                    #add it to the new_ladder
                 if node == end:
-                    if path:                              #if that path is not None merge the ladders
+                    if path:                                #if that path is not None merge the ladders
                         path.MergeLadders(new_ladder)
                     else:
-                        path = new_ladder                 #else put the path equal to the new_ladder
-                elif node in explored_level:  # check if the word has been explored
-                    explored_level[node].MergeLadders(new_ladder)     #merge the ladders
+                        path = new_ladder                   #else put the path equal to the new_ladder
+                elif node in explored_level:                # check if the word has been explored
+                    explored_level[node].MergeLadders(new_ladder)      #merge the ladders
                 else:
                     explored_level[node] = new_ladder
                     queue.append(new_ladder)
-        if path:                  #if the path is not None
+        if path:                                                       #if the path is not None
             break
-        explored |= explored_level.keys()  #union of explored with explored_level.keys
+        explored |= explored_level.keys()                              #union of explored with explored_level.keys
     if path:
         return path.FinalLadder()
 
@@ -93,13 +93,13 @@ if __name__ == '__main__':
     print()
 
     #Examples of errors:
-    print(BFS("joke", "happiness", WordsDictionary))  #The words are not of the same length.
+    print(BFS("joke", "happiness", WordsDictionary))            #The words are not of the same length.
     print()
     print(BFS("asseverating", "longitudinal", WordsDictionary)) #There is no path that connects these words.
     print()
-    print(BFS("hrgaar", "turtle", WordsDictionary))   #The starting word is not a valid word from the dictionary.
+    print(BFS("hrgaar", "turtle", WordsDictionary))             #The starting word is not a valid word from the dictionary.
     print()
-    print(BFS("magazine", "magazine", WordsDictionary))   #The start and end words are the same.
+    print(BFS("magazine", "magazine", WordsDictionary))         #The start and end words are the same.
     print()
     #Time to ask the user for input of start word and end word:
     print("Lets play the Word Ladders Game!")
